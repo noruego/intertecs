345cd7744ddffb68118103329280a0ad7d0c3c8b
adrian <adrian.ed@hotmail.es>
2018-11-08 02:36:53 -0600

    update 7/10/18 master detail in progress

--- Intertecs/Intertecs/App.xaml.cs
+++ Intertecs/Intertecs/App.xaml.cs
@@ -8,12 +8,25 @@ namespace Intertecs
 {
 	public partial class App : Application
 	{
-		public App ()
-		{
-			//InitializeComponent();
+        //Titulo de nombre del deporte 
+        public static String SportTitle { get; set; }
+        //Número de disciplina que mandara al ws paa visualizar
+        public static int SportNumber { get; set; }
+        // Variable que indica que TabbedPage esta viendo el usuario
+        public static int StatusDisciplina{ get; set; }
+        //Tipo de competencia (individual o de conjunto)
+        public static int tipoCompetencia { get; set; }
 
-			MainPage = new SplashPage();
-		}
+        public App ()
+		{
+            //Valores default
+            App.SportTitle = "Fútbol";
+            App.SportNumber = 5;
+            App.StatusDisciplina = 2;
+            App.tipoCompetencia = 2;
+            
+            MainPage = new SplashPage();
+        }
 
 		protected override void OnStart ()
 		{
